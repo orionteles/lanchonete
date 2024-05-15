ac08a73da6acc327703e438c76767b8a9bf0febe
Orion Teles de Mesquita <orionteles@gmail.com>
2024-05-15 19:30:53 -0300

    CRUD

--- app/controllers/ingredientes_controller.ts
+++ app/controllers/ingredientes_controller.ts
@@ -1,4 +1,40 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
+import Ingrediente from "../models/ingrediente.js"
+
 export default class IngredientesController {
+
+    index(){
+        return Ingrediente.all()
+    }
+
+    show({params}){
+        const id = params.id
+        return Ingrediente.query().where('id', id).first()
+    }
+    
+    async store({request}){
+        const dados = request.only(['nome'])
+        return await Ingrediente.create(dados)
+    }
+    
+    async update({request, params}){
+        const dados = request.only(['nome'])
+
+        const id = params.id
+        const filme = await Ingrediente.findOrFail(id)
+        
+        filme.merge(dados)
+        await filme.save()
+
+        return filme
+    }
+    
+    async destroy({params}){
+        const id = params.id
+        const filme = await Ingrediente.findOrFail(id)
+        
+        filme.delete()
+    }
+
 }
